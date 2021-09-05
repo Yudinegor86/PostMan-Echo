5563716210da0f72dc913f381583a1b841c69bc2
Yudin Egor <yudin.egor26@gmail.com>
2021-09-06 04:40:47 +0500

    Lets PASS

--- src/test/java/LetsPOST.java
+++ src/test/java/LetsPOST.java
@@ -15,7 +15,7 @@ public class LetsPOST {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
